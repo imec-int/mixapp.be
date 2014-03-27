93344db1ec80dd27d84d4662bf3eda4b719270a3
Sam Decrock <sam.decrock@gmail.com>
2014-03-27 11:10:11 +0100

    first working site

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-mixapp.be
-=========
+# mixapp.be
 
 Site die online zal draaien als de mensen toch via 3G/Edge naar mixapp.be surfen
+
+## Werking
+
+Als ze naar mixlab.be surfen, dan krijgen de genodigden de boodschap dat ze zich met het wifi-netwerk moeten verbinden.
+
+Als ze met het wifi-netwerk verbonden zijn, zullen ze langs onze lokale DNS-server passeren die hen zal vertellen dat mixapp.be een lokaal ip-adres is. De lokale DNS-server staat namelijk binnen het AB-netwerk.
+
+
+Mochten ze **via de wifi** voor één of andere reden toch op de online versie van mixapp.be terechtkomen (door DNS caching), dan wordt online toch nog eens gecheckt of ze niet met het IP van de AB aan het surfen zijn. In dat geval worden ze geredirect naar ab.mixlab.be, die sowieso naar het lokaal ip-adres binnen de AB verwijst.
+
+### TTL's op Amazon Route 53
+Ik stel voor om de TTL's op Amazon Route 53 zo laag mogelijk te zetten. Zo blijft het ip adres niet te lang in hun lokale DNS cache zitten en komen ze, na het verbinden met de wifi, mooi op de lokale versie van mixapp.be terecht.
+
+### Dummy pagina
+Momenteel zit hier een dummypagina in die de lokale mixapp.be (alsook ab.mixapp.be) moet voorstellen.Dat is nu gewoon om te testen.
+
+
+
---
+++ app/app.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var http = require('http')
+var path = require('path');
+var config = require('./config');
+var utils = require('./utils');
+
+var app = express();
+
+app.configure(function(){
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('mixapp123456789987654321'));
+	app.use(express.session());
+	app.use(app.router);
+	app.use(require('stylus').middleware(__dirname + '/public'));
+	app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler());
+});
+
+var webserver = http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
+
+app.get('/', function (req, res){
+	if( req.get('host') == 'ab.mixlab.be' ) return res.render('dummy'); // om nu ff te testen, mag weg later
+
+
+	// http://stackoverflow.com/questions/8107856/how-can-i-get-the-users-ip-address-using-node-js
+	var ip = req.headers['x-forwarded-for'] ||
+		req.connection.remoteAddress ||
+		req.socket.remoteAddress ||
+		req.connection.socket.remoteAddress;
+
+	console.log(ip);
+
+	// je bent op de wifi als je ip dat van de AB is (moeten we nog zien uit te vissen wat dat is dinsdag):
+	// maar normaal gezien mag je hier dan niet komen aangezien onze lokale DNS server jou naar een lokaal ip zou moeten gestuurd hebben,
+	// maarja, je weet nooit
+	var onwifi = (ip == config.abIP);
+
+	// in dat geval sturen we je naar ab.mixlab.be die zowel op Amazon DNS als op onze lokale DNS zal verwijzen naar een lokaal ip (dat van de regie-app)
+	if(onwifi) return res.redirect('http://ab.mixlab.be');
+
+
+	res.render('index', {
+		title: 'mixapp.be',
+		ip: ip
+	});
+});
+
